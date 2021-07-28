1f7e79bda4ad0ff28c3288f7d804aa56c3741504
unknown <iden.ticlla@gmail.com>
2021-07-28 10:33:01 -0400

    finalizando app

--- App.js
+++ App.js
@@ -1,72 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
-import { FlatList, StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TextInput,
+} from "react-native";
 
-import CoinItem from './components/CoinItem';
+import CoinItem from "./components/CoinItem";
 
 const App = () => {
-    const [coins, setCoins] = useState()
+  const [coins, setCoins] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [search, setSearch] = useState("");
 
-    const loadData = async() => {
-        const res = await fetch(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        );
-        const data = await res.json();
-        console.log(data);
-        setCoins(data)
-    }
-    useEffect(() => {
-        console.log('loaded')
-        loadData()
-    },[])
+  const loadData = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    setCoins(data);
+  };
 
-    return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor='#141414'/>
-            <View style={styles.header}>
-                <Text style={styles.title}>CryptoMarket</Text>
-                <TextInput style={styles.searchInput}/>
-            </View>
-            <FlatList
-                style={styles.list}
-                data={coins}
-                renderItem={({item})=> {
-                    console.log(item)
-                    return <CoinItem coin={item}/>
-                }}
+  useEffect(() => {
+    loadData();
+  }, []);
 
-                showsVerticalScrollIndicator= {false}
-            />
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#141414" />
+
+      <View style={styles.header}>
+        <Text style={styles.title}>CryptoMarket</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a Coin"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => text && setSearch(text)}
+        />
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={coins.filter(
+          (coin) =>
+            coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+            coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <CoinItem coin={item} />}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          setRefreshing(true);
+          await loadData();
+          setRefreshing(false);
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#141414",
-    alignItems: "center",
     flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+    marginBottom: 10,
   },
   title: {
-    color: "#fff",
-    marginTop: 20,
     fontSize: 20,
+    color: "#fff",
+    marginTop: 10,
   },
   list: {
     width: "90%",
   },
-  header: {
-    flexDirection: "row",
-    justifyContent: 'space-between',
-    width: '90%'
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
   },
-  searchInput:{
-      color: '#fff',
-      borderBottomColor: '#4657CE',
-      borderBottomWidth: 1,
-      width: '40%',
-      textAlign: 'center'
-  }
 });
 
-export default App
+export default App;
